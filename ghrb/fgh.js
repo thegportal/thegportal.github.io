11cfb719526acf8b328acd498109c150df6de415
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-03-25 19:46:37 -0500

    Update fgh.js

--- ghrb/fgh.js
+++ ghrb/fgh.js
@@ -1,4 +1,4 @@
-fetch('https://thegportal.github.io/ghrb/gtxt.txt')
+fetch('https://thegportal.github.io/ghrb/index.html')
   .then(response => {
     if (!response.ok) throw new Error('Network response was baddd~');
     return response.text();
