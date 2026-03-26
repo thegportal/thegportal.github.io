5bb6233373a564d7ffebff41b65cbbff37082608
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-03-25 19:09:02 -0500

    Update fgh.js

--- ghrb/fgh.js
+++ ghrb/fgh.js
@@ -1,4 +1,4 @@
-fetch('https://thegportal.github.io/ghrb/gtxt.txt')
+fetch('https://thegportal.github.io/ghrb/gtxt.txtt')
   .then(response => {
     if (!response.ok) throw new Error('Network response was baddd~');
     return response.text();
@@ -7,4 +7,8 @@ fetch('https://thegportal.github.io/ghrb/gtxt.txt')
     console.log(data);
     document.body.innerHTML = data;
   })
-  .catch(error => console.error('Fetch error: ', error, ' :('));
+  .catch(error => {
+    console.error('Fetch error: ', error, ' :(');
+    document.body.innerHTML = `<p>Failed to fetch the GHub data  :(<br>Report a problem:</p><br>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScbnf5yjlLtawLyMVYepb8VwwCwgtV4fABgQOi0eBD3_E2npQ/viewform?embedded=true" width="640" height="425">Loading…</iframe>`;
+  });
