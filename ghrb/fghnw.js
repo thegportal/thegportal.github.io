e02445a9d1cf2de5131d85333e0ec07c215d8adc
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-04-06 20:09:41 -0500

    Update fghnw.js

--- ghrb/fghnw.js
+++ ghrb/fghnw.js
@@ -1,15 +1,18 @@
-fetch('https://thegportal.github.io/ghrb/index.html')
-  .then(response => {
-    if (!response.ok) throw new Error('Network response was baddd~');
-    return response.text();
-  })
-  .then(data => {
-    console.log(data);
-    window.open('/').document.write(data);
-    document.body.innerHTML = '<p>This window can now be closed :)</p>';
-  })
-  .catch(error => {
-    console.error('Fetch error: ', error, ' :(');
-    document.body.innerHTML = `<p style="margin: 0; font-family: Helvetica;">Failed to fetch the GHub data  :(<br>Report a problem:</p>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScbnf5yjlLtawLyMVYepb8VwwCwgtV4fABgQOi0eBD3_E2npQ/viewform?embedded=true" width="640" height="425" style="border: 2px solid black; border-radius: 5px;">Loading…</iframe>`;
-  });
+function fetgh() {
+  fetch('https://thegportal.github.io/ghrb/index.html')
+    .then(response => {
+      if (!response.ok) throw new Error('Network response was baddd~');
+      return response.text();
+    })
+    .then(data => {
+      console.log(data);
+      window.open('/').document.write(data);
+      document.body.innerHTML = `<p>This window can now be closed. If a new window hasn't opened, click the button below :)</p>
+      <button onclick="fetgh()">Click me!</button>`;
+    })
+    .catch(error => {
+      console.error('Fetch error: ', error, ' :(');
+      document.body.innerHTML = `<p style="margin: 0; font-family: Helvetica;">Failed to fetch the GHub data  :(<br>Report a problem:</p>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScbnf5yjlLtawLyMVYepb8VwwCwgtV4fABgQOi0eBD3_E2npQ/viewform?embedded=true" width="640" height="425" style="border: 2px solid black; border-radius: 5px;">Loading…</iframe>`;
+    });
+}
