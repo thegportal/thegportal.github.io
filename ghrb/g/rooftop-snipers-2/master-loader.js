2b0e2e21af09709940a3d62212549c5a1a677ef2
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-07-09 15:55:03 -0500

    Update master-loader.js

--- ghrb/g/rooftop-snipers-2/master-loader.js
+++ ghrb/g/rooftop-snipers-2/master-loader.js
@@ -27,5 +27,5 @@ if (!window.config.unityWebglLoaderUrl) {
 var sdkScript = document.createElement("script");
 sdkScript.src = "https://thegportal.github.io/ghrb/g/rooftop-snipers-2/poki-sdk.js", sdkScript.onload = function() {
     var i = document.createElement("script");
-    i.src = loader, document.body.appendChild(i)
+    i.src = "https://raw.githubusercontent.com/thegportal/thegportal.github.io/refs/heads/main/ghrb/g/rooftop-snipers-2/unity.js", document.body.appendChild(i)
 }, document.body.appendChild(sdkScript);
