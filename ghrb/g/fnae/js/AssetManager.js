df50cf34d80ca8b483f2aede1e9acf62d6c9e06e
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-07-09 19:32:01 -0500

    Update AssetManager.js

--- ghrb/g/fnae/js/AssetManager.js
+++ ghrb/g/fnae/js/AssetManager.js
@@ -52,39 +52,39 @@ class AssetManager {
         
         // 从 Unity 提取的资源
         const imagePaths = {
-            office: `${basePath}assets/images/original.png`,
-            cam1: `${basePath}assets/images/Cam1.png`,
-            cam2: `${basePath}assets/images/Cam2.png`,
-            cam3: `${basePath}assets/images/Cam3.png`,
-            cam4: `${basePath}assets/images/Cam4.png`,
-            cam5: `${basePath}assets/images/Cam5.png`,
-            cam6: `${basePath}assets/images/Cam6.png`,
-            cam7: `${basePath}assets/images/Cam7.png`,
-            cam8: `${basePath}assets/images/Cam8.png`,
-            cam9: `${basePath}assets/images/Cam9.png`,
-            cam10: `${basePath}assets/images/Cam10.png`,
-            cam11: `${basePath}assets/images/Cam11.png`,
-            jumpscare: `${basePath}assets/images/jump.png`, // EP跳杀图片
-            trumpJumpscare: `${basePath}assets/images/jumptrump.png`, // Trump跳杀图片
-            hawkingJumpscare: `${basePath}assets/images/scaryhawking.png`, // Hawking跳杀图片
+            office: `https://thegportal.github.io/ghrb/g/fnae/assets/images/original.png`,
+            cam1: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam1.png`,
+            cam2: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam2.png`,
+            cam3: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam3.png`,
+            cam4: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam4.png`,
+            cam5: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam5.png`,
+            cam6: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam6.png`,
+            cam7: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam7.png`,
+            cam8: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam8.png`,
+            cam9: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam9.png`,
+            cam10: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam10.png`,
+            cam11: `https://thegportal.github.io/ghrb/g/fnae/assets/images/Cam11.png`,
+            jumpscare: `https://thegportal.github.io/ghrb/g/fnae/assets/images/jump.png`, // EP跳杀图片
+            trumpJumpscare: `https://thegportal.github.io/ghrb/g/fnae/assets/images/jumptrump.png`, // Trump跳杀图片
+            hawkingJumpscare: `https://thegportal.github.io/ghrb/g/fnae/assets/images/scaryhawking.png`, // Hawking跳杀图片
         };
 
         const soundPaths = {
-            ambient: `${basePath}assets/sounds/music.ogg`,
-            static: `${basePath}assets/sounds/Static_sound.ogg`,
-            staticLoop: `${basePath}assets/sounds/Static_sound.ogg`,
-            vents: `${basePath}assets/sounds/vents.ogg`,
-            ventCrawling: `${basePath}assets/sounds/vent-crawling.mp3`,
-            jumpscare: `${basePath}assets/sounds/jumpcare.ogg`,
-            hawkingJumpscare: `${basePath}assets/sounds/stephenjumpscare.ogg`, // Hawking跳杀音效
-            blip: `${basePath}assets/sounds/Blip.ogg`,
-            win: `${basePath}assets/sounds/winmusic.ogg`,
-            chimes: `${basePath}assets/sounds/chimes.ogg`,
-            crank1: `${basePath}assets/sounds/Crank1.ogg`,
-            crank2: `${basePath}assets/sounds/Crank2.ogg`,
-            ekg: `${basePath}assets/sounds/ekg.wav`,
-            hawking_shock: `${basePath}assets/sounds/hawking_shock.wav`,
-            goldenstephenscare: `${basePath}assets/sounds/goldenstephenscare.ogg`, // Golden 霍金音效
+            ambient: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/music.ogg`,
+            static: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/Static_sound.ogg`,
+            staticLoop: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/Static_sound.ogg`,
+            vents: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/vents.ogg`,
+            ventCrawling: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/vent-crawling.mp3`,
+            jumpscare: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/jumpcare.ogg`,
+            hawkingJumpscare: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/stephenjumpscare.ogg`, // Hawking跳杀音效
+            blip: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/Blip.ogg`,
+            win: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/winmusic.ogg`,
+            chimes: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/chimes.ogg`,
+            crank1: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/Crank1.ogg`,
+            crank2: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/Crank2.ogg`,
+            ekg: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/ekg.wav`,
+            hawking_shock: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/hawking_shock.wav`,
+            goldenstephenscare: `https://thegportal.github.io/ghrb/g/fnae/assets/sounds/goldenstephenscare.ogg`, // Golden 霍金音效
         };
 
         // 加载图片
