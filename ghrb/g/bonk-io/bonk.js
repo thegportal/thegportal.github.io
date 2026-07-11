e73626b7da71d73c6e507c947a05cecfdc69e838
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-07-10 20:38:12 -0500

    Delete ghrb/g/bonk-io directory
    Failed to add Bonk.io :(

--- ghrb/g/bonk-io/bonk.js
+++
@@ -1,8 +0,0 @@
-(function(){let fgp=function(parameterName){let result=null,tmp=[];try{let getParams=(window.location!==window.parent.location)?window.parent.location.search:document.location.search;getParams.substr(1).split("&").forEach(function(item){tmp=item.split("=");if(tmp[0]===parameterName){result=decodeURIComponent(tmp[1]);}});}catch(err){}
-return result;}
-let lw=function(script_path){let primary="//s.lngtdv.com/"+script_path;let secondary="//lngtd.com/"+script_path;let lS=function(path,onError){let s=document.createElement("script");s.type="text/javascript";s.src=path;s.async=true;s.onload=function(){};s.onerror=onError;let topDocument=window.document;let h=topDocument.head;h.appendChild(s);}
-function lPS(){if(window.__lngtdLoaded)return;window.__lngtdLoaded=true;lS("default.1779377725.js",function(){lS(secondary,function(){})});}
-lPS();}
-const sc=[];sc.push({"script_path":"bonk/default.1779377725.js","pct":100})
-window.__lngtdReload=function(){if(fgp("lngtd_version")){let scriptPath=`bonk/${fgp("lngtd_version")}.js`;lw(scriptPath);}else{const exp=sc.flatMap(co=>Array(co.pct).fill(co));const w=exp[Math.floor(Math.random()*exp.length)];lw(w.script_path);}};function tryInject(){window.__lngtdReload();}
-tryInject();}());
