835b2ef63ccd7a56b3a5d4e0a03c8777e93a2367
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-07-28 15:17:34 -0500

    Add files via upload

--- ghrb/g/monkey-mart/dmloader.js
+++ ghrb/g/monkey-mart/dmloader.js
@@ -342,7 +342,7 @@ var GameArchiveLoader = {
 
         var total = 0;
         var downloaded = 0;
-        var url = this._archiveLocationFilter('/' + piece.name);
+        var url = piece.name;
 
         FileLoader.load(
             url, "arraybuffer", undefined,
