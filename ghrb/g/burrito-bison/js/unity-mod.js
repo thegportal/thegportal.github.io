3346ba14c806666283086dfe931220811f8e4211
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-08-04 16:23:27 -0500

    Update unity-mod.js

--- ghrb/g/burrito-bison/js/unity-mod.js
+++ ghrb/g/burrito-bison/js/unity-mod.js
@@ -1,7 +1,7 @@
 function UnityUrlFix(url) {
 	console.log("--fx--UnityUrlFix--", url);
 	if(url.indexOf("unity3d.com")>0 || url.indexOf("appspot.com")>0){
-		url= "json/null.json?"+ url;
+		url= "https://raw.githubusercontent.com/thegportal/thegportal.github.io/refs/heads/main/ghrb/g/burrito-bison/json/null.json?"+ url;
 	}
 	return url;
 }
