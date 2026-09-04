93683c981de9ec5faccdf0fa4bd9b658c72b9849
Jeremy Powell <135190385+thegportal@users.noreply.github.com>
2026-09-03 21:24:34 -0500

    Update fghnw.js

--- ghrb/fghnw.js
+++ ghrb/fghnw.js
@@ -1,12 +1,12 @@
 function fetgh() {
-  fetch('https://thegportal.github.io/ghrb/index.html')
+  fetch('https://raw.githubusercontent.com/thegportal/thegportal.github.io/refs/heads/main/ghrb/index.html')
     .then(response => {
       if (!response.ok) throw new Error('Network response was baddd~');
       return response.text();
     })
     .then(data => {
       console.log(data);
-      window.open('/').document.write(data);
+      window.open('a:lmao').document.write(data);
       document.body.innerHTML = `<p>This window can now be closed. If a new window hasn't opened, click the button below :)</p>
       <button onclick="fetgh()">Click me!</button>`;
     })
